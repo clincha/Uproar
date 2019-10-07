8f563492b39fabbe670858e5615a89a2b738a8e5
A.G.L.Clinch <aglc2@kent.ac.uk>
2019-10-07 13:45:42 +0100

    Initial commit

---
+++ src/main/java/com/uproar/UproarApplication.java
@@ -0,0 +1,13 @@
+package com.uproar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UproarApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UproarApplication.class, args);
+  }
+
+}
