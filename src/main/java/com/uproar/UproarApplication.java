ee878ba5974a4300d6d12f6ccb497c858a9d03f2
A.G.L.Clinch <aglc2@kent.ac.uk>
2019-10-11 15:06:35 +0100

    hello sam

--- src/main/java/com/uproar/UproarApplication.java
+++ src/main/java/com/uproar/UproarApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UproarApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(UproarApplication.class, args);
   }
-
 }
---
+++ src/main/java/com/uproar/controller/mainController.java
@@ -0,0 +1,8 @@
+package com.uproar.controller;
+
+/**
+ * @author Angus Clinch
+ * @version 11/10/2019
+ **/
+public class mainController {
+}
