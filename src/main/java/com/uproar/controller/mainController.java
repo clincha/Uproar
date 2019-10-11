b66006197c14ffdf33ac5b1efe88352ec2323cc6
A.G.L.Clinch <aglc2@kent.ac.uk>
2019-10-11 15:35:07 +0100

    hello sam

--- src/main/java/com/uproar/controller/mainController.java
+++ src/main/java/com/uproar/controller/mainController.java
@@ -1,8 +1,18 @@
 package com.uproar.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * @author Angus Clinch
  * @version 11/10/2019
  **/
+@Controller
 public class mainController {
+
+  @GetMapping("/")
+  public String homepage() {
+    return "Angus";
+  }
+
 }
