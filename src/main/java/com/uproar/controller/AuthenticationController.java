781b45f54bfe08c160fbf653989376a7b47cb7ed
A.G.L.Clinch <aglc2@kent.ac.uk>
2019-11-04 19:48:52 +0000

    Templates for login pages

---
+++ src/main/java/com/uproar/controller/AuthenticationController.java
@@ -0,0 +1,15 @@
+package com.uproar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AuthenticationController {
+
+  @RequestMapping("/login")
+  public ModelAndView login() {
+    return new ModelAndView("authentication/login");
+  }
+
+}
