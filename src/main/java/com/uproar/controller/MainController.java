20cd01339d1e91bd86ded478ba3b2c1cccad77de
A.G.L.Clinch <aglc2@kent.ac.uk>
2019-11-20 14:39:54 +0000

    Basic page template has been done and there is an example too

--- src/main/java/com/uproar/controller/MainController.java
+++ src/main/java/com/uproar/controller/MainController.java
@@ -10,7 +10,7 @@ public class MainController {
   @GetMapping("/")
   public ModelAndView homepage() {
 
-    return new ModelAndView("Society");
+    return new ModelAndView("exampleTemplate");
   }
 
 
