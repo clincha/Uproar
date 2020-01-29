65dd6b0bbd07a8d2a218afaea49800fcd6b31a5d
aclinch <NvnJgDvyvtckB4Fhmj6QCCZlkZb2kd>
2020-01-29 18:44:38 +0000

    sso working with GitHub

--- src/main/java/com/uproar/config/securityConfig.java
+++ src/main/java/com/uproar/config/securityConfig.java
@@ -1,45 +1,23 @@
 package com.uproar.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class securityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication()
-      .withUser("user1@uproar.com").password(passwordEncoder().encode("password")).roles("USER").and()
-      .withUser("user2@uproar.com").password(passwordEncoder().encode("password")).roles("USER").and()
-      .withUser("admin@uproar.com").password(passwordEncoder().encode("password")).roles("ADMIN");
-  }
-
-  @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-      .csrf().disable()
       .authorizeRequests()
-      .antMatchers("/ticket/**").hasRole("USER")
-      .antMatchers("/**").permitAll()
+      .antMatchers("/ticket/**").authenticated()
+      .antMatchers("/", "/login").permitAll()
+      .antMatchers("/css/**", "/js/**", "/image/**").permitAll()
       .anyRequest().authenticated()
       .and()
-      .formLogin()
-      .loginPage("/login")
-      .failureUrl("/loginError")
-      .and()
-      .logout()
-      .logoutUrl("/logout");
-  }
-
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
+      .oauth2Login();
   }
 }
