cedbcede471268280b160510f7e315594638651b
aclinch <NvnJgDvyvtckB4Fhmj6QCCZlkZb2kd>
2020-03-09 14:34:29 +0000

    Authentication with three users

--- src/main/java/com/uproar/configuration/WebSecurityConfig.java
+++ src/main/java/com/uproar/configuration/WebSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
 
