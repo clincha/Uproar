73fd039f528ee328dc8a3fe6221b424fd54f194a
A.G.L.Clinch <aglc2@kent.ac.uk>
2020-03-11 11:49:18 +0000

    Barcode is working now

--- src/main/resources/static/js/barcode.js
+++ src/main/resources/static/js/barcode.js
@@ -1,6 +1,5 @@
 $(".barcode-holder").each(function () {
     let barcodeText = $(this).find("#barcode-value")[0].value;
-    console.log(barcodeText);
     $(this).find("#barcode").JsBarcode(barcodeText, {
         displayValue: true,
         height: 20
